8367bb5ae56fd9433c96f7b1fd689ff94fd513d2
Alexander Popov <apopov@caltech.edu>
2019-03-11 20:53:18 -0700

    Trained LSTM models

--- main.py
+++ main.py
@@ -80,14 +80,14 @@ def train_LSTM(X, y, v_size, temp):
     cb = [callbacks.EarlyStopping(monitor='loss', min_delta=0, patience=2, verbose=0, mode='auto')]
 
     model = Sequential()
-    model.add(layers.LSTM(100, input_shape=(X.shape[1], X.shape[2])))
+    model.add(layers.LSTM(200, input_shape=(X.shape[1], X.shape[2])))
     model.add(layers.Dropout(0.2))
     model.add(layers.Lambda(lambda x: x/temp))
     model.add(layers.Dense(v_size, activation='softmax'))
     model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
     print(model.summary())
     print("train!")
-    history = model.fit(X, y, epochs = 100, verbose = 2, callbacks = cb)
+    history = model.fit(X, y, epochs = 150, verbose = 2, callbacks = cb)
     print("done!")
     model.save('model.tmp')
     return model
@@ -169,19 +169,19 @@ if __name__ == '__main__':
 
     if (LSTM):
         print("running LSTM")
-        X, y, v_size, char_to_token, token_to_char = get_LSTM_data("data/shakespeare.txt", True, 5)
+        X, y, v_size, char_to_token, token_to_char = get_LSTM_data("data/shakespeare.txt", True, 2)
         #model = train_LSTM(X, y, v_size)
         model1 = train_LSTM(X, y, v_size, 0.25)
-        model1.save('lstm_t25.model')
+        model1.save('lstm_t25_2.model')
 
         model2 = train_LSTM(X, y, v_size, 0.75)
-        model2.save('lstm_t75.model')
+        model2.save('lstm_t75_2.model')
 
         model3 = train_LSTM(X, y, v_size, 1.50)
-        model3.save('lstm_t150.model')
+        model3.save('lstm_t150_2.model')
 
         #model = models.load_model('m1.model')
-        print(generate_seq(model, char_to_token, token_to_char, "shall i compare thee to a summer's day?\n"))
+        print(generate_seq(model1, char_to_token, token_to_char, "shall i compare thee to a summer's day?\n"))
 
     if (LSTM_embed):
         print("running word embedded LSTM")
