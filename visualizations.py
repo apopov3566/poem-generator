31e513297a009826bec2a86ef0c4ea9825524c07
Andrew Hess <ahess3815@gmail.com>
2019-03-14 11:10:05 -0700

    Added printing of most common words from each state

--- visualizations.py
+++ visualizations.py
@@ -1,6 +1,8 @@
 import os
+import numpy as np
 from joblib import load
 import matplotlib.pyplot as plt
+from data_handler import get_corpus
 from HMM_helper import (
     text_to_wordcloud,
     states_to_wordclouds,
@@ -11,6 +13,28 @@ from HMM_helper import (
 )
 
 
+def print_common_words(emissions, detoken, nwords):
+    '''
+    Print the nwords most common words given by the emissions array.
+
+    emissions (1d array): The probability that word i will be emitted is emissions[i].
+    detoken (dict: int -> string): Map the token of a word to the word itself.
+    nwords: The number of words to print.
+
+    returns: None
+    '''
+
+    # Find the tokens with the highest probability of occurring.
+    tokens = list(np.argpartition(emissions, -nwords)[-nwords:])
+
+    for token in tokens:
+        print(detoken[token], end='\t')
+
+    print()
+
+    return
+
+
 def main(model_file):
     ''' Run visualizations for the provided model. '''
 
@@ -19,7 +43,14 @@ def main(model_file):
     model = load(model_file)
 
     # Visualize the transition and observation matrices.
-    visualize_sparsities(model, O_max_cols=10000)
+    visualize_sparsities(model, O_max_cols=10000, O_vmax=0.003)
+
+    # Get the most common output words from each state.
+    _, detoken, _ = get_corpus("data/shakespeare.txt", split_by_line=False)
+
+    for state in range(model.L):
+        print(f'state {state} -> ', end='')
+        print_common_words(model.O[state], detoken, nwords=10)
 
     # Make  word cloud for each state.
     obs, obs_map = parse_observations(open(os.path.join(os.getcwd(), 'data/shakespeare.txt')).read())
